6cf981e68da879031e3db6c8924a43049eae479c
Anthony Tromboni <ant5389@gmail.com>
2021-05-07 23:23:05 -0400

    cards.js linked and ready for first submission

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const usersRouter = require('./routes/users.js');
+const cardsRouter = require('./routes/cards.js');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use('/', usersRouter);
+app.use('/', cardsRouter);
+
+app.get('*', (req, res) => {
+  res.status(404).send({ message: 'Requested resource not found' })
+});
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
