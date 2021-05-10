6affa0dde58819dc65ea9185a6b7918d1b3b9cb7
Anthony Tromboni <ant5389@gmail.com>
2021-05-09 23:01:47 -0400

    corrected res arguments in getDataFromFile

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,7 +4,7 @@ const getDataFromFile = require('../helpers/files.js');
 const dataPath = path.join(__dirname, '..', 'data', 'cards.json');
 
 const getCards = (req, res) => {
-  getDataFromFile(dataPath)
+  getDataFromFile(res, dataPath)
     .then(users => res.status(200).send(users))
     .catch(err => res.status(500).send(err));
 };
--- helpers/files.js
+++ helpers/files.js
@@ -4,7 +4,7 @@ const getDataFromFile = (res, pathToFile) => {
   return fsPromises.readFile(pathToFile, { encoding: 'utf-8' })
     .then(data => JSON.parse(data))
     .catch(err => {
-      res.status(500).render('error', { message: err });
+      res.status(500).send({ message: err });
     });
 };
 
