3b76d662aefb38826418bd7c1e9d4add6ee1fe1e
Anthony Tromboni <ant5389@gmail.com>
2021-06-01 17:40:14 -0400

    corrected typo in route

--- routes/users.js
+++ routes/users.js
@@ -15,6 +15,6 @@ router.post('/users', createProfile);
 
 router.patch('/users/me', updateProfile);
 
-router.patch('users/me/avatar', updateAvatar);
+router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
